63752be5cff934fc38600e4e85db9c6e8ef521d9
server107 <71307167+server107@users.noreply.github.com>
2020-09-15 16:39:35 -0400

    Add files via upload

---
+++ geektext/src/test/java/com/geektextbookstore/geektext/GeektextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geektextbookstore.geektext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeektextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
